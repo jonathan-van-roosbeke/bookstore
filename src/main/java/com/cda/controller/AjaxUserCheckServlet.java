f84d27c5f9167b86755f21a481e7d44468e67218
Jonathan <jonathan.van.roosbeke@gmail.com>
2020-08-20 16:26:50 +0200

    spring mvc Ajax user check

--- src/main/java/com/cda/controller/AjaxUserCheckServlet.java
+++ src/main/java/com/cda/controller/AjaxUserCheckServlet.java
@@ -2,40 +2,46 @@ package com.cda.controller;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cda.entity.Utilisateur;
 import com.cda.service.IUtilisateurService;
 
-@WebServlet("/user-check")
-public class AjaxUserCheckServlet extends AbstractController {
+@Controller
+public class AjaxUserCheckServlet {
 	
-	private static final long serialVersionUID = 1L;
 	
 	@Autowired
 	IUtilisateurService utilisateurService;
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String login = request.getParameter("login").trim();
+	@GetMapping(value = "/user-check")
+	public void validationAjout(
+			@RequestParam(value = "login") String login,
+			HttpServletResponse response) {
+		
 		Utilisateur utilisateur = utilisateurService.findById(login);
 		System.out.println(utilisateur);
 		
 		if(utilisateur != null) {
 			response.setContentType("text/plain");
-			response.getWriter().write(utilisateur.getLogin() + " existe déjà");
-			response.setStatus(400);
+			try {
+				response.getWriter().write(utilisateur.getLogin() + " existe déjà");
+				response.setStatus(400);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		} else {
-			response.setContentType("text/plain");
-			response.getWriter().write("valide");
+			try {
+				response.setContentType("text/plain");
+				response.getWriter().write("valide");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}		
 	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	}
-
 }
