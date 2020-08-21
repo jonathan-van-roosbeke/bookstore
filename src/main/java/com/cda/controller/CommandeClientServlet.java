88900e3d2beabdfdbd4a86ae8ba32b2ae85d86c4
BARGUI <riadh.bargui@gmail.com>
2020-08-21 15:42:25 +0200

    regler probleme passer commande

--- src/main/java/com/cda/controller/CommandeClientServlet.java
+++ src/main/java/com/cda/controller/CommandeClientServlet.java
@@ -63,6 +63,7 @@ public class CommandeClientServlet {
 	 * @throws ServletException
 	 * @throws IOException
 	 */
+	@GetMapping(value = "/checkout")
 	protected String checkout(HttpSession session) {
 		log.info("checkout");
 		Utilisateur loginUtilisateur = (Utilisateur) session.getAttribute("utilisateur");
